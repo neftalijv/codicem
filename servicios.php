be54513aa8144e7150288de5e582e29b6586f0e6
Ivan Valdez <ivan_3034@hotmail.com>
2013-10-05 19:53:26 -0500

    [0003]Agregando seccion de Servicios

---
+++ servicios.php
@@ -0,0 +1,43 @@
+	
+				<section class="caja">
+				<hgroup>
+				<h1>
+				Titulo 1
+				</h1>
+				</hgroup>
+				<figure><img id="servicios1" src="images/servicios1.jpg"/ ></figure>
+				<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas posuere neque at molestie sodales. Aenean aliquam elit mi, eget elementum nisl varius non. Nunc non fermentum orci. Vestibulum eget diam semper, porta nibh ac, semper odio. Fusce ipsum arcu, suscipit ac pulvinar quis, malesuada a magna. Aliquam arcu tellus, auctor eget tincidunt eu, tincidunt et ligula. Nunc dictum nec enim tincidunt mattis. Ut a suscipit justo. Nullam sed libero lacinia, venenatis urna ac, adipiscing augue. Nam et orci ac dolor accumsan hendrerit id at libero. Nam eu rutrum purus. Phasellus sodales velit a sagittis placerat. Nullam vitae accumsan turpis. Nulla bibendum nunc eget dui consectetur dignissim. Pellentesque aliquam varius porttitor. Praesent ut dolor at felis porta suscipit quis posuere massa.
+				</p>
+			    </section>
+
+				<section class="caja">
+				<hgroup>
+				<h1>
+				Titulo 2
+				</h1>
+				</hgroup>
+				<figure><img id="servicios2" src="images/servicios2.jpg"/ ></figure>
+				<p>Suspendisse potenti. Suspendisse nec dui justo. Sed aliquam mauris eu risus dapibus convallis. Vestibulum gravida lacus id justo lacinia, vitae pulvinar arcu feugiat. Curabitur molestie gravida vestibulum. Nam cursus a diam vel aliquet. Morbi bibendum orci sed ipsum pretium volutpat. Etiam in tincidunt orci. Sed sit amet eros suscipit, mattis arcu sed, blandit dui. Nunc et aliquam diam, vel bibendum mi. Sed bibendum, velit cursus rhoncus volutpat, elit sapien adipiscing diam, sit amet feugiat urna ipsum nec nulla. Quisque interdum ipsum eget orci congue, imperdiet lacinia enim auctor. Curabitur interdum id sem a rutrum. Curabitur pellentesque risus vel urna dapibus fermentum. Ut fermentum, leo sit amet viverra suscipit, est urna consectetur leo, sit amet egestas sem elit in massa.
+				</p>
+				</section>
+
+				<section class="caja">
+				<hgroup>
+				<h1>
+				Titulo 3
+				</h1>
+				</hgroup>
+				<figure><img id="servicios3" src="images/servicios3.jpg"/ ></figure>
+				<p>Praesent sed placerat metus. Vivamus nec nisl dui. Nulla viverra mollis tellus. Nam vestibulum vitae mauris ac vehicula. Vestibulum eleifend fringilla tortor, nec bibendum justo mollis quis. Proin imperdiet turpis arcu, a rhoncus nunc rhoncus ullamcorper. Nam bibendum est non nunc tincidunt, ut euismod justo convallis. Donec sed nisl sem. Mauris porttitor nunc at malesuada dictum. Praesent hendrerit odio non sodales pharetra. Nunc id laoreet tortor. Phasellus imperdiet neque vitae lacus dignissim, vitae placerat mi semper. Suspendisse eu accumsan nunc.
+				</p>
+				</section>
+				<section class="caja">
+				<hgroup>
+				<h1>
+				Titulo 4
+				</h1>
+				</hgroup>
+				<figure><img id="servicios4" src="images/servicios4.jpg"/ ></figure>
+				<p>Nunc in facilisis nulla. Cras consectetur dolor nec lectus malesuada, eget aliquam magna ornare. Nunc imperdiet venenatis metus ut facilisis. Morbi aliquet laoreet lorem in eleifend. Curabitur at dapibus diam. Morbi facilisis est volutpat justo eleifend dictum. Nullam vel congue risus. Suspendisse eu dolor non nisi tristique ultrices sed sagittis nisi. In bibendum, justo eu congue ultricies, enim mauris dignissim magna, at mollis dolor mi quis urna.</p>
+				</section>			
+		
